239ddd61c3fd720fb3d3a27285dcbdc924615d1f
samcat437 <sdrees12@gmail.com>
2022-04-03 18:01:37 +0000

    added js file, wrote questions array

---
+++ static/js/game.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+      "question": "What major key is this?",
+      "image": "",
+      "options": [
+        "A major", "C major", "E major", "F# major"
+      ],
+      "answer" : "E major"
+    },
+    
+    {
+        "question": "What major key is this?",
+        "image": "",
+        "options": [
+          "F major", "G major", "Eb major", "D major"
+        ],
+        "answer" : "F major"
+    },
+
+    {
+        "question": "What minor key is this?",
+        "image": "",
+        "options": [
+          "Bb minor", "C minor", "Db minor", "Ab minor"
+        ],
+        "answer" : "Ab minor"
+    },
+
+    {
+        "question": "What major key is this?",
+        "image": "",
+        "options": [
+          "C major", "E major", "F major", "C# major"
+        ],
+        "answer" : "C# major"
+    },
+
+    {
+        "question": "What minor key is this?",
+        "image": "",
+        "options": [
+            "F minor", "D minor", "C minor", "Eb minor"
+        ],
+        "answer" : "Eb minor"
+    },
+
+    {
+        "question": "What major key is this?",
+        "image": "",
+        "options": [
+          "D major", "A major", "B major", "F# major"
+        ],
+        "answer" : "A major"
+    },
+
+    {
+        "question": "What minor key is this?",
+        "image": "",
+        "options": [
+            "B minor", "G minor", "E minor", "F minor"
+        ],
+        "answer" : "F minor"
+    },
+
+    {
+        "question": "What minor key is this?",
+        "image": "",
+        "options": [
+            "Ab minor", "B minor", "G minor", "C# minor"
+        ],
+        "answer" : "G minor"
+    },
+
+    {
+        "question": "What major key is this?",
+        "image": "",
+        "options": [
+          "C# major", "D major", "B major", "A major"
+        ],
+        "answer" : "A major"
+    },
+
+    {
+        "question": "What minor key is this?",
+        "image": "",
+        "options": [
+            "Bb minor", "C minor", "A minor", "F minor"
+        ],
+        "answer" : "C minor"
+    },
+    
+  ]; 
