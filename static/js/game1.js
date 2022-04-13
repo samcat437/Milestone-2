245247cf4ee74a9bc797a712d048c69376dd9d44
samcat437 <sdrees12@gmail.com>
2022-04-13 18:29:28 +0000

    quiz showing questions, startGame and newQuestions are working, next will be to add functionality to click an answer and check the answer

--- static/js/game1.js
+++ static/js/game1.js
@@ -1,161 +1,111 @@
 //Variables
+const image= document.getElementById('image-container');
 const question = document.querySelector('#question');
 const options = Array.from(document.querySelectorAll('.option-text'));
 const startButton = document.getElementById('start');
+const nextButton = document.getElementById('next');
+const welcome = document.getElementById('welcome');
 
 let currentQuestion = {};
 let availableQuestions = [];
+let n = 0;
 
 const questions = [{
     question: "What major key is this?",
     image: `<img src="../static/images/e-major.png"/>`,
-    options: {
-      a: "A major",
-      b: "C major",
-      c: "E major",
-      d: "F# major"
-    },
-    answer: "c"
+    options: ["A major","C major","E major", "F# major"],
+    answer: "E major"
   },
 
   {
     question: "What major key is this?",
     image: `<img src="../static/images/f-major.png"/>`,
-    options: {
-      a: "F major",
-      b: "G major",
-      c: "Eb major",
-      d: "D major"
-    },
-    answer: "a"
+    options: ["F major", "G major", "Eb major", "D major"],
+    answer: "F major"
   },
 
   {
     question: "What minor key is this?",
     image: `<img src="../static/images/c-flat-major.png"/>`,
-    options: {
-      a: "Bb minor",
-      b: "C minor",
-      c: "Db minor",
-      d: "Ab minor"
-    },
-    answer: "d",
+    options: ["Bb minor", "C minor", "Db minor", "Ab minor"],
+    answer: "Ab minor"
   },
 
   {
     question: "What major key is this?",
     image: `<img src="../static/images/c-sharp-major.png"/>`,
-    options: {
-      a: "C major",
-      b: "E major",
-      c: "F major",
-      d: "C# major"
-    },
-    answer: "d"
+    options: ["C major", "E major", "F major", "C# major"],
+    answer: "C# major"
   },
 
   {
     question: "What minor key is this?",
     image: `<img src="../static/images/g-flat-major.png"/>`,
-    options: {
-      a: "F minor",
-      b: "D minor",
-      c: "C minor",
-      d: "Eb minor"
-    },
-    answer: "d"
+    options: ["F minor", "D minor", "C minor", "Eb minor"],
+    answer: "Eb minor"
   },
 
   {
     question: "What major key is this?",
     image: `<img src="../static/images/a-major.png"/>`,
-    options: {
-      a: "D major",
-      b: "A major",
-      c: "B major",
-      d: "F# major"
-    },
-    answer: "b"
+    options:  ["D major", "A major", "B major", "F# major"],
+    answer: "A major"
   },
 
   {
     question: "What minor key is this?",
     image: `<img src="../static/images/a-flat-major.png"/>`,
-    options: {
-      a: "B minor",
-      b: "G minor",
-      c: "E minor",
-      d: "F minor"
-    },
-    answer: "d"
+    options: ["B minor", "G minor", "E minor", "F minor"],
+    answer: "F minor"
   },
 
   {
     question: "What minor key is this?",
     image: `<img src="../static/images/b-flat-major.png"/>`,
-    options: {
-      a: "Ab minor",
-      b: "B minor",
-      c: "G minor",
-      d: "C# minor"
-    },
-    answer: "c"
+    options: ["Ab minor", "B minor", "G minor", "C# minor"],
+    answer: "G minor"
   },
 
   {
     question: "What major key is this?",
     image: `<img src="../static/images/e-major.png"/>`,
-    options: {
-      a: "C# major",
-      b: "D major",
-      c: "B major",
-      d: "A major"
-    },
-    answer: "c"
+    options: ["C# major", "D major", "B major", "A major"],
+    answer: "B major"
   },
 
   {
     question: "What minor key is this?",
     image: `<img src="../static/images/e-flat-major.png"/>`,
-    options: {
-      a: "Bb minor",
-      b: "C minor",
-      c: "A minor",
-      d: "F minor"
-    },
-    answer: "b"
+    options: ["Bb minor", "C minor", "A minor", "F minor"],
+    answer: "C minor"
   }
 
 ];
 
 startGame = () => {
-
+  welcome.style.display = 'none';
+  startButton.style.display = 'none';
   newQuestion();
 };
 
 function newQuestion() {
-  // let n = 0;
-  
-  //   question.innerHTML = questions[n].question;
-
-  //   for (let i = 0; i < questions.length; i++) {
-  //     let option = options[n];
-  //     option.innerHTML = questions.options;
-  //     n++;
-  //   }
 
   const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex];
-    let n = 0;
+    
+    image.innerHTML = questions[n].image;
+    
     question.innerHTML = questions[n].question;
 
-    options.forEach(option => {
-        option.innerHTML = currentQuestion;
-    });
+    for (let i = 0; i < 4; i++) {
+
+      options[i].innerHTML = questions[n].options[i];
+      };
 
     availableQuestions.splice(questionsIndex, 1);
     n++;
 
   };
 
-  startGame();
+  startButton.addEventListener('click', startGame);
+  nextButton.addEventListener('click', newQuestion);
